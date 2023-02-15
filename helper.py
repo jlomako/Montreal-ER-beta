7701b609dfab4b330dad75494e2f6bcca41ae2df
J Lomako <jlomako@web.de>
2023-02-15 14:38:30 -0500

    add helper functions

---
+++ helper.py
@@ -0,0 +1,11 @@
+import pandas as pd
+def get_data():
+    url = "https://github.com/jlomako/hospital-occupancy-tracker/raw/main/tables/patients_total.csv"
+    df = pd.read_csv(url, parse_dates=['Date']).drop_duplicates('Date')
+    df['Date'] = pd.DatetimeIndex(df['Date']).floor('H') + pd.Timedelta(minutes=46) # set all to 46min
+    # create df_range with timestamps for every hour
+    date_range = pd.date_range(start=df['Date'].min(), end=df['Date'].max(), freq='H')
+    df_range = pd.DataFrame({'Date': date_range})
+    # merge with df
+    df = pd.merge(df_range, df, on='Date', how='outer')
+    return df
--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 import streamlit as st
-import pandas as pd
 import plotly.express as px
+from helper import get_data
 # terminal: streamlit run main.py
-
-def get_data():
-    url = "https://github.com/jlomako/hospital-occupancy-tracker/raw/main/tables/patients_total.csv"
-    df = pd.read_csv(url, parse_dates=['Date']).drop_duplicates('Date')
-    df['Date'] = pd.DatetimeIndex(df['Date']).floor('H') + pd.Timedelta(minutes=46) # set all to 46min
-    # create df with timestamps for every hour
-    date_range = pd.date_range(start=df['Date'].min(), end=df['Date'].max(), freq='H')
-    df_range = pd.DataFrame({'Date': date_range})
-    df = pd.merge(df_range, df, on='Date', how='outer')
-    return df
 
 
 df = get_data()
+
 hospitals = list(df.columns[1::])
 
 st.title("Patients waiting in ER")
@@ -22,16 +13,17 @@ st.title("Patients waiting in ER")
 selected = st.selectbox("Select ER", hospitals)
 # st.subheader(f"{selected}")
 
-df = df.filter(items=['Date', selected])
+df_hospital = df.filter(items=['Date', selected])
+df_hospital.rename(columns={selected: 'total_patients'}, inplace=True)
 
 
 # plot
-fig = px.line(df, x='Date', y=selected, title=selected)
+fig = px.line(df_hospital, x='Date', y='total_patients', title=selected)
 fig.update_layout(xaxis_tickmode='linear', xaxis_dtick='1D')
 st.plotly_chart(fig)
 
 # scatter
-fig1 = px.scatter(df, x='Date', y=selected,  title=selected, opacity=0.7)
+fig1 = px.scatter(df_hospital, x='Date', y='total_patients',  title=selected, opacity=0.7)
 fig1.update_traces(marker=dict(size=3, line=dict(width=1)))
 fig1.update_layout(xaxis_tickmode='linear', xaxis_dtick='1D')
 st.plotly_chart(fig1)
