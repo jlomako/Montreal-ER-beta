b27f49a05fcd2b869c2f316c8be0ea25404e0c94
J Lomako <jlomako@web.de>
2023-02-16 11:51:01 -0500

    filter timestamps for last 7 days

--- helper.py
+++ helper.py
@@ -1,11 +1,13 @@
+from datetime import datetime, timedelta
 import pandas as pd
 import streamlit as st
 
 @st.cache_data
 def load_data(url):
-#    url = "https://github.com/jlomako/hospital-occupancy-tracker/raw/main/tables/patients_total.csv"
     df = pd.read_csv(url, parse_dates=['Date']).drop_duplicates('Date')
     df['Date'] = pd.DatetimeIndex(df['Date']).floor('H') + pd.Timedelta(minutes=46) # set all to 46min
+    # filter last 7 days
+    df = df[(df["Date"] >= (df['Date'].max() - timedelta(days=7))) & (df["Date"] <= df['Date'].max())]
     # create df_range with timestamps for every hour
     date_range = pd.date_range(start=df['Date'].min(), end=df['Date'].max(), freq='H')
     df_range = pd.DataFrame({'Date': date_range})
